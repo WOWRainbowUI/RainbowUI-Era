5834ca627eece007cc9e93131f20660e056a61e1
class2u <class2u@gmail.com>
2023-12-24 13:56:33 +0800

    Add Auctionator 10.2.3

---
+++ AddOns/Auctionator/Source_Vanilla/Initialize/Main.lua
@@ -0,0 +1,43 @@
+local AUCTIONATOR_EVENTS = {
+  "CRAFT_SHOW",
+}
+
+AuctionatorInitializeVanillaMixin = {}
+
+function AuctionatorInitializeVanillaMixin:OnLoad()
+  FrameUtil.RegisterFrameForEvents(self, AUCTIONATOR_EVENTS)
+end
+
+function AuctionatorInitializeVanillaMixin:OnEvent(event, ...)
+  if event == "CRAFT_SHOW" then
+    Auctionator.EnchantInfo.Initialize()
+    self:CraftShown()
+  end
+end
+
+function AuctionatorInitializeVanillaMixin:CraftShown()
+  Auctionator.Debug.Message("AuctionatorInitializeVanillaMixin::CraftShown()")
+
+  if self.initializedCraftHooks then
+    return
+  end
+
+  local reagentHook = function(self)
+    if IsModifiedClick("CHATLINK") and AuctionatorShoppingFrame ~= nil and AuctionatorShoppingFrame:IsVisible() then
+      local name = GetCraftReagentInfo(GetCraftSelectionIndex(), self:GetID())
+
+      if name == nil then
+        return
+      end
+
+      AuctionatorShoppingFrame.OneItemSearchBox:SetText(name)
+      AuctionatorShoppingFrame.OneItemSearchButton:Click()
+    end
+  end
+  CraftReagent1:HookScript("OnClick", reagentHook)
+  CraftReagent2:HookScript("OnClick", reagentHook)
+  CraftReagent3:HookScript("OnClick", reagentHook)
+  CraftReagent4:HookScript("OnClick", reagentHook)
+
+  self.initializedCraftHooks = true
+end
