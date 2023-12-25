aeb0ee85ac7a096538a20676bb1206be68cfc6b5
class2u <class2u@gmail.com>
2023-12-25 09:54:29 +0800

    更新 採集助手-資料庫 v2023-11-29

--- AddOns/GatherMate2_Data/GatherMateData.lua
+++ AddOns/GatherMate2_Data/GatherMateData.lua
@@ -1,7 +1,7 @@
 -- nearby check yes/no? slowdown may be an isue if someone leaves the mod enabled and always replace node
 local GatherMateData = LibStub("AceAddon-3.0"):NewAddon("GatherMate2_Data")
 local GatherMate = LibStub("AceAddon-3.0"):GetAddon("GatherMate2")
-GatherMateData.generatedVersion = "531"
+GatherMateData.generatedVersion = "493"
 
 function GatherMateData:PerformMerge(dbs,style, zoneFilter)
 	local filter = nil -- Removed expansion filters
@@ -9,7 +9,6 @@ function GatherMateData:PerformMerge(dbs,style, zoneFilter)
 	if dbs["Herbs"]    then self:MergeHerbs(style ~= "Merge",filter) end
 	if dbs["Fish"]     then self:MergeFish(style ~= "Merge",filter) end
 	if dbs["Treasure"] then self:MergeTreasure(style ~= "Merge",filter) end
-	if dbs["Gases"]    then self:MergeGas(style ~= "Merge",filter) end
 	self:CleanupImportData()
 	GatherMate:SendMessage("GatherMateData2Import")
 	--GatherMate:CleanupDB()
@@ -20,7 +19,7 @@ function GatherMateData:MergeMines(clear,zoneFilter)
 	for zoneID, node_table in pairs(GatherMateData2MineDB) do
 		if not zoneFilter or zoneFilter[zoneID] then
 			for coord, nodeID in pairs(node_table) do
-				GatherMate:InjectNode2(zoneID,self:CheckCoord(zoneID,coord),"Mining", nodeID)
+				GatherMate:InjectNode2(zoneID,coord,"Mining", nodeID)
 			end
 		end
 	end
@@ -31,7 +30,7 @@ function GatherMateData:MergeHerbs(clear,zoneFilter)
 	for zoneID, node_table in pairs(GatherMateData2HerbDB) do
 		if not zoneFilter or zoneFilter[zoneID] then
 			for coord, nodeID in pairs(node_table) do
-				GatherMate:InjectNode2(zoneID,self:CheckCoord(zoneID,coord),"Herb Gathering", nodeID)
+				GatherMate:InjectNode2(zoneID,coord,"Herb Gathering", nodeID)
 			end
 		end
 	end
@@ -42,7 +41,7 @@ function GatherMateData:MergeFish(clear,zoneFilter)
 	for zoneID, node_table in pairs(GatherMateData2FishDB) do
 		if not zoneFilter or zoneFilter[zoneID] then
 			for coord, nodeID in pairs(node_table) do
-				GatherMate:InjectNode2(zoneID,self:CheckCoord(zoneID,coord),"Fishing", nodeID)
+				GatherMate:InjectNode2(zoneID,coord,"Fishing", nodeID)
 			end
 		end
 	end
@@ -53,18 +52,7 @@ function GatherMateData:MergeTreasure(clear,zoneFilter)
 	for zoneID, node_table in pairs(GatherMateData2TreasureDB) do
 		if not zoneFilter or zoneFilter[zoneID] then
 			for coord, nodeID in pairs(node_table) do
-				GatherMate:InjectNode2(zoneID,self:CheckCoord(zoneID,coord),"Treasure", nodeID)
-			end
-		end
-	end
-end
-
-function GatherMateData:MergeGas(clear,zoneFilter)
-	if clear then GatherMate:ClearDB("Extract Gas") end
-	for zoneID, node_table in pairs(GatherMateData2GasDB) do
-		if not zoneFilter or zoneFilter[zoneID] then
-			for coord, nodeID in pairs(node_table) do
-				GatherMate:InjectNode2(zoneID,self:CheckCoord(zoneID,coord),"Extract Gas", nodeID)
+				GatherMate:InjectNode2(zoneID,coord,"Treasure", nodeID)
 			end
 		end
 	end
@@ -75,16 +63,4 @@ function GatherMateData:CleanupImportData()
 	GatherMateData2MineDB = nil
 	GatherMateData2FishDB = nil
 	GatherMateData2TreasureDB = nil
-	GatherMateData2GasDB = nil
-end
-
-function GatherMateData:AdjustEPL(id)
-	local mulX, mulY, shiftX, shiftY = 0.959165, 0.953350, -0.0252, -0.0303
-	local x,y,c = floor(id / 1e6 * mulX) / 10000 + shiftX, floor(id % 1e6 / 100 * mulY) / 10000 + shiftY, id % 100
-	return floor(x * 1e10 + y * 1e6 + c)
-end
-
-function GatherMateData:CheckCoord(id,coord)
-	local EPL = 1423
-	return id == EPL and self:AdjustEPL(coord) or coord
 end
