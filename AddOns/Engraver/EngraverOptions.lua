7f3f11030d251fd27387841084b433b8f93bf11c
class2u <class2u@gmail.com>
2024-01-23 10:36:39 +0800

    更新 一鍵符文 v1.12.8

--- AddOns/Engraver/EngraverOptions.lua
+++ AddOns/Engraver/EngraverOptions.lua
@@ -2,12 +2,16 @@ local localAddonName, Addon = ...
 local EngraverDisplayModes = Addon.EngraverDisplayModes 
 
 -- Slash Commands
-SLASH_ENGRAVER1, SLASH_ENGRAVER2, SLASH_ENGRAVER3, SLASH_ENGRAVER4, SLASH_ENGRAVER5 = "/en", "/eng", "/engrave", "/engraver", "/engraving"
+SLASH_ENGRAVER1, SLASH_ENGRAVER2, SLASH_ENGRAVER3, SLASH_ENGRAVER4, SLASH_ENGRAVER5, SLASH_ENGRAVER6, SLASH_ENGRAVER7 = "/en", "/eng", "/eg", "/re", "/engrave", "/engraver", "/engraving"
 SlashCmdList.ENGRAVER = function(msg, editBox)
 	Settings.OpenToCategory(localAddonName);
 end
 
 EngraverOptions = {} -- SavedVariable
+EngraverOptionsCallbackRegistry = CreateFromMixins(CallbackRegistryMixin)
+EngraverOptionsCallbackRegistry:OnLoad()
+EngraverOptionsCallbackRegistry:SetUndefinedEventsAllowed(true)
+
 EngraverOptionsFrameMixin = {}
 
 function EngraverOptionsFrameMixin:OnLoad()
@@ -40,31 +44,37 @@ local DefaultEngraverOptions = {
 	LayoutDirection = 0,
 	HideTooltip = false,
 	HideDragTab = false,
-	EnableRightClickDrag = true,
-	UIScale = 1.0
+	EnableRightClickDrag = false,
+	UIScale = 1.0,
+	ShowFilterSelector = false,
+	CurrentFilter = 0
 }
 
-function EngraverOptionsFrameMixin:CreateSettingsInitializers()
-	self.settings = {}
-	self.initializers = {}
-	local function addSetting(variable, name, varType)
-		local setting = Settings.RegisterAddOnSetting(self.category, name, variable, varType, DefaultEngraverOptions[variable]);
-		self.settings[variable] = setting
-		Settings.SetOnValueChangedCallback(variable, function (_, _, newValue, ...)
-			EngraverOptions[variable] = newValue;
-			EngraverOptions:TriggerEvent(variable, newValue)
-		end, self)
-		return setting
-	end
-	local function addInitializer(initializer)
-		if initializer then	
-			table.insert(self.initializers, initializer);
+local function AddEngraverOptionsSetting(self, variable, name, varType)
+	local setting = Settings.RegisterAddOnSetting(self.category, name, variable, varType, DefaultEngraverOptions[variable]);
+	self.engraverOptionsSettings[variable] = setting
+	Settings.SetOnValueChangedCallback(variable, function (_, _, newValue, ...)
+		EngraverOptions[variable] = newValue;
+		EngraverOptionsCallbackRegistry:TriggerEvent(variable, newValue)
+	end, self)
+	return setting
+end
+
+local function AddInitializer(self, initializer)
+	if initializer then	
+		table.insert(self.initializers, initializer);
+		if initializer.GetName then
 			initializer:AddSearchTags(initializer:GetName():gmatch("%S+"))
 		end
 	end
+end
+
+function EngraverOptionsFrameMixin:CreateSettingsInitializers()
+	self.engraverOptionsSettings = {}
+	self.initializers = {}
 	do -- DisplayMode
 		local variable, name, tooltip = "DisplayMode", "Rune Display Mode", "Rune Display Mode";
-		local setting = addSetting(variable, name, Settings.VarType.Number)
+		local setting = AddEngraverOptionsSetting(self, variable, name, Settings.VarType.Number)
 		local options = function()
 			local container = Settings.CreateControlTextContainer();
 			for i, displayMode in ipairs(Addon.EngraverDisplayModes) do
@@ -72,11 +82,11 @@ function EngraverOptionsFrameMixin:CreateSettingsInitializers()
 			end
 			return container:GetData();
 		end
-		addInitializer(Settings.CreateDropDownInitializer(setting, options, tooltip))
+		AddInitializer(self, Settings.CreateDropDownInitializer(setting, options, tooltip))
 	end -- DisplayMode
 	do -- LayoutDirection
 		local variable, name, tooltip = "LayoutDirection", "Layout Direction", "Layout Direction";
-		local setting = addSetting(variable, name, Settings.VarType.Number)
+		local setting = AddEngraverOptionsSetting(self, variable, name, Settings.VarType.Number)
 		local options = function()
 			local container = Settings.CreateControlTextContainer();
 			for i, direction in ipairs(Addon.EngraverLayoutDirections) do
@@ -84,24 +94,31 @@ function EngraverOptionsFrameMixin:CreateSettingsInitializers()
 			end
 			return container:GetData();
 		end
-		addInitializer(Settings.CreateDropDownInitializer(setting, options, tooltip))
+		AddInitializer(self, Settings.CreateDropDownInitializer(setting, options, tooltip))
 	end -- LayoutDirection
 	do -- HideTooltip
-		addInitializer(Settings.CreateCheckBoxInitializer(addSetting("HideTooltip", "Hide Tooltip", Settings.VarType.Boolean), nil, "Hides the tooltip when hovering over a rune button."))
+		AddInitializer(self, Settings.CreateCheckBoxInitializer(AddEngraverOptionsSetting(self, "HideTooltip", "Hide Tooltip", Settings.VarType.Boolean), nil, "Hides the tooltip when hovering over a rune button."))
 	end -- HideTooltip
 	do -- HideDragTab
-		addInitializer(Settings.CreateCheckBoxInitializer(addSetting("HideDragTab", "Hide Drag Tab", Settings.VarType.Boolean), nil, "Hides the drag tab of the Engraver frame."))
+		AddInitializer(self, Settings.CreateCheckBoxInitializer(AddEngraverOptionsSetting(self, "HideDragTab", "Hide Drag Tab", Settings.VarType.Boolean), nil, "Hides the drag tab of the Engraver frame."))
 	end -- HideDragTab
 	do -- EnableRightClickDrag
-		addInitializer(Settings.CreateCheckBoxInitializer(addSetting("EnableRightClickDrag", "Enable Right Click Drag", Settings.VarType.Boolean), nil, "Enables dragging the frame by right-clicking and holding any rune button."))
+		AddInitializer(self, Settings.CreateCheckBoxInitializer(AddEngraverOptionsSetting(self, "EnableRightClickDrag", "Enable Right Click Drag", Settings.VarType.Boolean), nil, "Enables dragging the frame by right-clicking and holding any rune button."))
 	end -- EnableRightClickDrag
 	do -- UIScale
 		local variable, name, tooltip = "UIScale", "UI Scale", "Adjusts the scale of the Engraver's user interface frame.";
-		local setting = addSetting(variable, name, Settings.VarType.Number)
+		local setting = AddEngraverOptionsSetting(self, variable, name, Settings.VarType.Number)
 		local options = Settings.CreateSliderOptions(0.01, 2.5, 0.00) -- minValue, maxValue, step 
 		options:SetLabelFormatter(MinimalSliderWithSteppersMixin.Label.Right, FormatPercentage);
-		addInitializer(Settings.CreateSliderInitializer(setting, options, tooltip))
+		AddInitializer(self, Settings.CreateSliderInitializer(setting, options, tooltip))
 	end	-- UIScale
+	do -- ShowFilterSelector
+		AddInitializer(self, Settings.CreateCheckBoxInitializer(AddEngraverOptionsSetting(self, "ShowFilterSelector", "Show Filter Selector", Settings.VarType.Boolean), nil, "Shows the filter selector."))
+	end -- ShowFilterSelector
+	AddInitializer(self, CreateSettingsListSectionHeaderInitializer("Filters"));
+	do -- FilterEditor
+		table.insert(self.initializers, Settings.CreateElementInitializer("EngraverOptionsFilterEditorTemplate", { settings = {} } ));
+	end -- FilterEditor
 end
 
 function EngraverOptionsFrameMixin:OnEvent(event, ...)
@@ -122,9 +139,7 @@ function EngraverOptionsFrameMixin:OnDefault()
 end
 
 function EngraverOptionsFrameMixin:SetOptionsToDefault(force)
-	EngraverOptions = CreateFromMixins(EngraverOptions or {}, CallbackRegistryMixin)
-	EngraverOptions:OnLoad()
-	EngraverOptions:SetUndefinedEventsAllowed(true)
+	EngraverOptions = EngraverOptions or {}
 	for k, v in pairs(DefaultEngraverOptions) do
 		if force or EngraverOptions[k] == nil then
 			if type(v) == "table" then 
@@ -136,13 +151,11 @@ function EngraverOptionsFrameMixin:SetOptionsToDefault(force)
 end
 
 function EngraverOptionsFrameMixin:OnRefresh()
-	if self.settings then
-		for variable, setting in pairs(self.settings) do
-			setting:SetValue(EngraverOptions[variable])
+	if self.engraverOptionsSettings then
+		for variable, setting in pairs(self.engraverOptionsSettings) do
+			if setting.SetValue then
+				setting:SetValue(EngraverOptions[variable])
+			end
 		end
 	end
 end
-
---function EngraverOptionsFrameMixin:OnCommit()
---	print("OnCommit")
---end
