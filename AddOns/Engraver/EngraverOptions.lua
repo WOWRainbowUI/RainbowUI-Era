33b6970de0854c5e174fe5ac090e28bd02357be5
class2u <class2u@gmail.com>
2024-01-29 13:21:20 +0800

    更新 一鍵符文 v1.12.13

--- AddOns/Engraver/EngraverOptions.lua
+++ AddOns/Engraver/EngraverOptions.lua
@@ -12,6 +12,25 @@ EngraverOptionsCallbackRegistry = CreateFromMixins(CallbackRegistryMixin)
 EngraverOptionsCallbackRegistry:OnLoad()
 EngraverOptionsCallbackRegistry:SetUndefinedEventsAllowed(true)
 
+Addon.EngraverVisibilityModes = {
+	["ShowAlways"] = { text = "Show Always", tooltip = "Engraver will always be visible." },
+	["HideInCombat"] = { text = "Hide in Combat", tooltip = "Show/Hide when combat ends/starts." },
+	["SyncCharacterPane"] = { text = "Sync with Character Pane", tooltip = "Show/Hide when you open/close your Character Pane." },
+}
+
+local DefaultEngraverOptions = {
+	DisplayMode = 1,
+	LayoutDirection = 0,
+	VisibilityMode = "ShowAlways",
+	HideTooltip = false,
+	HideDragTab = false,
+	ShowFilterSelector = false,
+	HideSlotLabels = false,
+	EnableRightClickDrag = false,
+	UIScale = 1.0,
+	CurrentFilter = 0
+}
+
 EngraverOptionsFrameMixin = {}
 
 function EngraverOptionsFrameMixin:OnLoad()
@@ -39,17 +58,6 @@ function EngraverOptionsFrameMixin:InitSettingsList()
 	self.settingsList:Show();
 end
 
-local DefaultEngraverOptions = {
-	DisplayMode = 1,
-	LayoutDirection = 0,
-	HideTooltip = false,
-	HideDragTab = false,
-	EnableRightClickDrag = false,
-	UIScale = 1.0,
-	ShowFilterSelector = false,
-	CurrentFilter = 0
-}
-
 local function AddEngraverOptionsSetting(self, variable, name, varType)
 	local setting = Settings.RegisterAddOnSetting(self.category, name, variable, varType, DefaultEngraverOptions[variable]);
 	self.engraverOptionsSettings[variable] = setting
@@ -73,19 +81,20 @@ function EngraverOptionsFrameMixin:CreateSettingsInitializers()
 	self.engraverOptionsSettings = {}
 	self.initializers = {}
 	do -- DisplayMode
-		local variable, name, tooltip = "DisplayMode", "Rune Display Mode", "Rune Display Mode";
+		local variable, name, tooltip = "DisplayMode", "Rune Display Mode", "How runes buttons are displayed.";
+		local tooltips = { "All runes are always shown.", "Show only one button for each engravable equipment slot. Move your cursor over any button to see the available runes." }
 		local setting = AddEngraverOptionsSetting(self, variable, name, Settings.VarType.Number)
 		local options = function()
 			local container = Settings.CreateControlTextContainer();
 			for i, displayMode in ipairs(Addon.EngraverDisplayModes) do
-				container:Add(i-1, displayMode.text);
+				container:Add(i-1, displayMode.text, tooltips[i]);
 			end
 			return container:GetData();
 		end
 		AddInitializer(self, Settings.CreateDropDownInitializer(setting, options, tooltip))
 	end -- DisplayMode
 	do -- LayoutDirection
-		local variable, name, tooltip = "LayoutDirection", "Layout Direction", "Layout Direction";
+		local variable, name, tooltip = "LayoutDirection", "Layout Direction", "Which direction the runes buttons are layed out.";
 		local setting = AddEngraverOptionsSetting(self, variable, name, Settings.VarType.Number)
 		local options = function()
 			local container = Settings.CreateControlTextContainer();
@@ -96,12 +105,36 @@ function EngraverOptionsFrameMixin:CreateSettingsInitializers()
 		end
 		AddInitializer(self, Settings.CreateDropDownInitializer(setting, options, tooltip))
 	end -- LayoutDirection
+	do -- VisibilityMode
+		local variable, name, tooltip = "VisibilityMode", "Visibility Mode", "When the Engraver is shown/hidden.";
+		local setting = AddEngraverOptionsSetting(self, variable, name, Settings.VarType.Number)
+		local options = function()
+			local container = Settings.CreateControlTextContainer();
+			for name, mode in pairs(Addon.EngraverVisibilityModes) do
+				container:Add(name, mode.text, mode.tooltip);
+			end
+			return container:GetData();
+		end
+		AddInitializer(self, Settings.CreateDropDownInitializer(setting, options, tooltip))
+	end -- VisibilityMode
 	do -- HideTooltip
 		AddInitializer(self, Settings.CreateCheckBoxInitializer(AddEngraverOptionsSetting(self, "HideTooltip", "Hide Tooltip", Settings.VarType.Boolean), nil, "Hides the tooltip when hovering over a rune button."))
 	end -- HideTooltip
 	do -- HideDragTab
-		AddInitializer(self, Settings.CreateCheckBoxInitializer(AddEngraverOptionsSetting(self, "HideDragTab", "Hide Drag Tab", Settings.VarType.Boolean), nil, "Hides the drag tab of the Engraver frame."))
+		local dragTabSetting = AddEngraverOptionsSetting(self, "HideDragTab", "Hide Drag Tab", Settings.VarType.Boolean)
+		local dragTabInitializer = Settings.CreateCheckBoxInitializer(dragTabSetting, nil, "The drag tab allows you to move the Engraver frame via mouse drag.")
+		AddInitializer(self, dragTabInitializer)
+		do -- ShowFilterSelector
+			local setting = AddEngraverOptionsSetting(self, "ShowFilterSelector", "Show Filter Selector", Settings.VarType.Boolean)
+			local initializer = Settings.CreateCheckBoxInitializer(setting, nil, "When enabled, the drag tab will display the active filter.\nArrow buttons are added to the drag tab for you to change the filter.")
+			initializer:SetParentInitializer(dragTabInitializer, function() return not dragTabSetting:GetValue() end)
+			initializer.IsParentInitializerInCurrentSettingsCategory = function() return true; end -- forces indent and small font
+			AddInitializer(self, initializer)
+		end -- ShowFilterSelector
 	end -- HideDragTab
+	do -- HideSlotLabels
+		AddInitializer(self, Settings.CreateCheckBoxInitializer(AddEngraverOptionsSetting(self, "HideSlotLabels", "Hide Slot Labels", Settings.VarType.Boolean)))
+	end -- HideSlotLabels
 	do -- EnableRightClickDrag
 		AddInitializer(self, Settings.CreateCheckBoxInitializer(AddEngraverOptionsSetting(self, "EnableRightClickDrag", "Enable Right Click Drag", Settings.VarType.Boolean), nil, "Enables dragging the frame by right-clicking and holding any rune button."))
 	end -- EnableRightClickDrag
@@ -112,10 +145,30 @@ function EngraverOptionsFrameMixin:CreateSettingsInitializers()
 		options:SetLabelFormatter(MinimalSliderWithSteppersMixin.Label.Right, FormatPercentage);
 		AddInitializer(self, Settings.CreateSliderInitializer(setting, options, tooltip))
 	end	-- UIScale
-	do -- ShowFilterSelector
-		AddInitializer(self, Settings.CreateCheckBoxInitializer(AddEngraverOptionsSetting(self, "ShowFilterSelector", "Show Filter Selector", Settings.VarType.Boolean), nil, "Shows the filter selector."))
-	end -- ShowFilterSelector
-	AddInitializer(self, CreateSettingsListSectionHeaderInitializer("Filters"));
+	do -- FiltersHeader
+		local filtersHeaderData = { 
+			name = "Filters", 
+			tooltipSections = {
+				{ 
+					header = "Creating a Filter", 
+					lines = {
+						"Create a new filter and give it an appropriate name.",
+						"Select runes that you want to see when the filter is active, the rest will be hidden."
+					}
+				},
+				{ 
+					header = "Activating a Filter", 
+					lines = {
+						"Only one filter (or none) can be active at a time.",
+						"The active filter is indicated by a green marker in the list below.",
+						"Right-click a filter in the list below to activate or deactivate it.",
+						"You can also use the Filter Selector (the arrow buttons on the drag tab) to change the active one. "
+					}
+				}
+			}
+		}
+		AddInitializer(self, Settings.CreateElementInitializer("SettingsListSectionHeaderWithInfoTemplate", filtersHeaderData));
+	end -- FiltersHeader
 	do -- FilterEditor
 		table.insert(self.initializers, Settings.CreateElementInitializer("EngraverOptionsFilterEditorTemplate", { settings = {} } ));
 	end -- FilterEditor
@@ -158,4 +211,30 @@ function EngraverOptionsFrameMixin:OnRefresh()
 			end
 		end
 	end
+end
+
+---------------------------------------
+-- SettingsListSectionHeaderWithInfo --
+---------------------------------------
+
+SettingsListSectionHeaderWithInfoMixin = CreateFromMixins(SettingsListSectionHeaderMixin)
+
+function SettingsListSectionHeaderWithInfoMixin:Init(initializer)
+	SettingsListSectionHeaderMixin.Init(self, initializer)
+	self.TooltipSections = initializer:GetData().tooltipSections
+end
+
+function SettingsListSectionHeaderInfoButton_OnEnter(self)
+	SettingsTooltip:SetOwner(self, "ANCHOR_RIGHT",-22,-22);
+	local sections = self:GetParent().TooltipSections
+	for i, section in ipairs(sections) do
+		GameTooltip_AddHighlightLine(SettingsTooltip, section.header);
+		for _, line in ipairs(section.lines) do
+			GameTooltip_AddNormalLine(SettingsTooltip, line);
+		end
+		if i < #sections then
+			GameTooltip_AddBlankLinesToTooltip(SettingsTooltip, 1)
+		end
+	end
+	SettingsTooltip:Show();
 end
